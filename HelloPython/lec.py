2178570dccbd73f76ef14496c5504320f12d9ce3
Galina <koroleva2525.gk@gmail.com>
2022-12-10 15:09:01 +0100

    add Hello Python

---
+++ HelloPython/lec.py
@@ -0,0 +1,36 @@
+## Типы данных и переменная
+## int, float, boolen, str, list, None
+# value = None
+# print(type(value))
+
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+#value = 12334
+#print(type(value))
+# s = "hello 'world"
+# s = 'hello "world'
+# s = 'hello \'world'
+#s = 'hello \nworld'
+# s = 'hello world'
+# print(s)
+# print(a,'-',b, '-',s)
+# print('{} - {} - {}'.format(a,b,s))
+# print('{1} - {2} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+
+#  #f = True
+#  f = False
+#  print(f)
+
+# list = ['1','2','3', 'hello']
+# print(list)
+
+print('Введите a');
+a = int(input())
+print('Введите b');
+b = int(input())
+print(a,' + ', b, ' = ', a +b)
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
