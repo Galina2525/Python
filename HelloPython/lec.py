44c30116edfb61935a9e606dc78e687b85dae57f
Galina <koroleva2525.gk@gmail.com>
2022-12-12 11:24:03 +0100

    changed HelloPython

--- HelloPython/lec.py
+++ HelloPython/lec.py
@@ -27,10 +27,190 @@
 # list = ['1','2','3', 'hello']
 # print(list)
 
-print('Введите a');
-a = int(input())
-print('Введите b');
-b = int(input())
-print(a,' + ', b, ' = ', a +b)
+# print('Введите a');
+# a = int(input())
+# print('Введите b');
+# b = int(input())
+# print(a,' + ', b, ' = ', a +b)
 # print('{} {}'.format(a, b))
 # print(f'{a} {b}')
+ 
+ # Арифметические операции
+ # +, -, *,/, %, //, **
+ # **,  ,  , *, /, //, %, +, -
+ 
+ 
+# a = 1.321451111
+# b = 3
+#c = a//b   # если хотим получить целое цисло от деления
+#c = a**b   # если хотим a возвести в степень b
+#c = round(a*b,7)   # если хотим  округлить до 7 знаков после запятой
+#print(c)
+
+#(), Сокращенные операции
+#a = 3
+#a = a + 5 или 
+#a += 5
+#print(a)
+
+#Логические операции
+# > , < , <= , >=, ==,!=
+# not, and, or   -  не путать с &, |, ^
+# is, is not, in, not in
+# gen
+# a = 1 < 4 and 5 > 2
+# print(a)
+
+# a = 'qwe'
+# b = 'qwe'
+# a ==b
+# print(a==b)
+
+# a = [1,2]
+# b = [1,2]
+# print(a==b)
+
+# a = 1 < 3 < 5 < 9
+# print(a)
+
+# func = 1
+# T = 4
+# x =123
+# print(func<T>(x))
+
+# f = 1 > 2 or 4 < 6 #истино т.к. хотя бы одно истино
+# print(f)
+
+#f =[1,2,3,4,]
+#print(f)
+# print( 2 in f)
+# print( not 2 in f)
+#is_odd = f[1] % 2 == 0 # f с индексом 1 это 2 в списке
+#print(is_odd)
+# по умолчанию ноль - это ложь, а единичка это истина поэтому вышесказанное можно записать
+# is_odd = not f[1] % 2
+# print(is_odd)
+
+#Управляющие конструкции
+# if, if -else
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print('максимальное число это ', a)
+# else:
+#     print('максимальное число это '  ,b)
+
+# username = input('Введите имя: ')
+# if username =='Галина':
+#     print('Ура! Это же Галина!')
+# elif username == ('Маша'):
+#     print('Я так долго Вас ждал, Маша!')
+# elif username == ('Андрей'):
+#     print('Как дела, Андрей?') 
+# else:
+#     print('Привет,', username,'!')          
+
+# Управляющие конструкции: while
+# Задача. Инвертировать число 23 в 32. По окончании написать "Пожалуй хватит"
+# original = 23
+# inverted = 0
+# while original !=0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //=10
+# else:
+#     print('Пожалуй')    
+#     print('хватит')    
+# print(inverted)    
+
+# Управляющие конструкции: for
+# for i in 1,2,3,4,5:
+#     print(i**2) квадрат каждого числа
+
+# list = {1,2,4,6,3}    
+# for i in list:
+#     print(i*2)   
+
+# r = range(10)
+# for i in r:
+#     print(i)
+
+# for i in range(1,5): # от 1 до 4
+#     print(i)
+  
+# for i in range (1,7,2):#от 1 до 6 с приращением 2
+#     print(i)
+
+# for i in 'qwerty':
+#     print(i)    
+
+#Работа с текстом
+#text ='съешь ещё этих мягких французских булок'
+#print(text[0])   #с (индекс 0)
+#print(text[1])   #ъ(индекс 1)
+#print(text[len(text)])   # IndexError
+#print(text[len(text)-1])   # к
+#print(text[-5])   # б - опускается len(text)
+# print(text[:])   # print(text) съешь ещё этих мягких французских булок
+# print(text[0:len(text)-1])   # print(text) съешь ещё этих мягких французских було
+# print(text[0:len(text)-5])   # print(text) съешь ещё этих мягких французских 
+# print(text[2:5])   # ешь 
+# print(text[0:5])   # съешь 
+# print(text[:5])   # съешь 
+# print(text[len(text)-1:])   # к
+# print(text[len(text)-3:])   # лок
+# print(text[2:9])   # ешь ещё
+# print(text[6:-18])   #  ещё этих мягких (длина строки или отрицательный символ)
+# print(text[0:len(text):6])   #  сеикакл
+# print(text[::6])   #  сеикакл
+
+# text = text[2:9] + text[-5] + text[:2]
+# print(text)
+
+# Списки: введение
+## list = list
+# numbers =[1,2,3,4,5,6]
+# ran = range(1,6)
+# print(type(ran)) #<class 'range'>
+# numbers = list(ran)
+# print(numbers)  #[1, 2, 3, 4, 5]
+
+# numbers =[1,2,3,4,5,6]
+# print(numbers)     #[1, 2, 3, 4, 5, 6]
+# numbers[0] = 10    #[10, 2, 3, 4, 5, 6]
+# print()
+# print(numbers)
+# for i in numbers:
+#     i *= 2
+#     print(i)
+# print(numbers)    
+
+# colors = ['red', 'green', 'blue']
+# for e in colors:
+#     print(e)  # red green blue
+
+# for e in colors:
+#     print(e*2)    #redred greengreen blueblue
+
+# colors.append('gray') # добавить в конец листа
+# print(colors ==['red', 'green', 'blue', 'gray'])   # True
+# colors.remove('red') # удаление элемента red
+# del colors[0] # удаление элемента red
+
+#Функции - это фрагмент программы, используемый многократно
+def f(x):
+    if x ==1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+# arg = 1  # Целое <class 'str'>
+# arg = 2.3  # 23 <class 'int'>
+
+arg = 2 # None <class 'NoneTipe'>
+print(f(arg))  
+print(type(f(arg)))  
+
+
+
